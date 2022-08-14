0cc9bb2e0aae0f9d6389b951d5bb64e9122e5970
COMPUTER\Пользователь <irusiksm@mail.ru>
2022-08-14 21:36:36 +0300

    Create all modules and classes.

--- store/src/main/java/by/issoft/store/Store.java
+++ store/src/main/java/by/issoft/store/Store.java
@@ -1,4 +1,16 @@
 package by.issoft.store;
 
+import by.issoft.domain.Category;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
+
+    private List<Category> categoryList = new ArrayList<>();
+
+    public void printAllCategoriesAndProducts() {
+        for (Category category : categoryList) {
+            category.printAllProduct();
+        }
+    }
 }
