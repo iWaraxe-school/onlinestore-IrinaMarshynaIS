d46310dff00b95b5efda0cfcda883c15545add4b
COMPUTER\Пользователь <irusiksm@mail.ru>
2022-08-16 15:13:45 +0300

    1. add details to RandomProductGenerator class 2. add details to RandomStorePopulator class.

--- consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
@@ -1,9 +1,13 @@
 package by.issoft.consoleApp;
 
+import by.issoft.store.RandomStorePopulator;
 import by.issoft.store.Store;
 
 public class StoreApp {
     public static void main(String[] args) {
+
         Store store = new Store();
+        RandomStorePopulator populator = new RandomStorePopulator(store);
+        populator.fillStoreRandomly();
     }
 }
