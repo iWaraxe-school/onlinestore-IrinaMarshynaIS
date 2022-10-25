6e40519d9f42060dd881c960d90209fe8e92e34b
IrinaMarshyna <irinamarshyna@coherentsolutions.com>
2022-10-25 11:58:01 +0300

    Find 5 most expensive products.

--- store/src/main/java/by/issoft/store/Store.java
+++ store/src/main/java/by/issoft/store/Store.java
@@ -1,13 +1,14 @@
 package by.issoft.store;
 
+import by.issoft.store.XML.SortingTypes.SortCategory;
+import by.issoft.store.XML.SortingTypes.SortType;
 import by.issoft.store.XML.XMLParser;
 import by.issoft.domain.Category;
 import by.issoft.domain.Product;
 import by.issoft.store.XML.Comparator.ProductComparator;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Store {
 
@@ -21,13 +22,30 @@ public class Store {
         Comparator<Product> productComparator = new ProductComparator(XMLParser.configMap());
         for (Category category : categoryList) {
             category.getProductList().sort(productComparator);
-
         }
     }
-
     public void printAllCategoriesAndProducts() {
         for (Category category : categoryList) {
             category.printAllProduct();
         }
     }
+    public void getTop5() {
+        System.out.println("_______________________________");
+        System.out.println("TOP 5");
+        System.out.println("_______________________________");
+
+        Map<SortCategory, SortType> sortTypeMap = new HashMap<>();
+        sortTypeMap.put(SortCategory.PRICE, SortType.DESCENDING);
+        Comparator<Product> productComparator = new ProductComparator(sortTypeMap);
+
+        productStream().sorted(productComparator)
+                .limit(5)
+                .forEach(System.out::println);
+        }
+
+        private Stream<Product> productStream(){
+        return categoryList.stream()
+                .map(Category::getProductList)
+                .flatMap(Collection::stream);
+        }
 }
